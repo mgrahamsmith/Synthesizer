4729d2346700e9a002899d18b859acca4509eaf8
Graham Smith <mgrahamsmith@protonmail.com>
2023-08-17 19:51:33 -0700

    Make sure olcNoiseMaker.h include works.

--- build.bat
+++ build.bat
@@ -1,13 +1,17 @@
 :: If old build folder is found, delete and rebuild
 @echo off
 if exist build (
+  @echo on
   rmdir /s build
 )
 
+:: Leave project root, create and enter build folder 
 md build
 cd build
 
+:: Compiler the project
 @echo on
 cl /EHsc ..\main.cpp
 
+:: Exit build folder back to project root.
 cd ..
